2ba34d2c7e0607e27c25baa1b83df32dee214926
Samuel Adekunle <samuelbeebest@gmail.com>
2025-07-22 18:30:16 +0100

    Initial Commit

---
+++ Whishlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whishlist
+//
+//  Created by Samuel Adekunle on 22/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whishlist/WhishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhishlistApp.swift
+//  Whishlist
+//
+//  Created by Samuel Adekunle on 22/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
